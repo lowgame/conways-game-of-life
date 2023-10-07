d96eabd7107ce971baa4d2684c9293273ae35704
Ahmet Kamer <kamobey17@gmail.com>
2023-10-07 21:59:52 +0300

    completed-game-files
    finished version of the game.
    
    note that: Explanations are in Turkish...

---
+++ sketch.js
@@ -0,0 +1,144 @@
+function make2DArray(cols, rows) {
+  let arr = new Array(cols);
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = new Array(rows);
+  }
+  return arr;
+}
+let cols = 10;
+let rows = 10;
+let resolution = 20;
+let isAnimating = false;
+let fps = "10";
+let h = 600;
+let w = 600;
+let generation = 0;
+
+function setup() {
+  var myCanvas = createCanvas(h, w);
+  myCanvas.parent("gridContainer");
+  cols = width / resolution;
+  rows = height / resolution;
+  grid = make2DArray(cols, rows);
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      grid[i][j] = 0;
+    }
+  }
+
+  let startButton = select("#startButton");
+  startButton.mousePressed(startDrawing);
+  let stopButton = select("#stopButton");
+  stopButton.mousePressed(stopDrawing);
+  let resetButton = select("#resetButton");
+  resetButton.mousePressed(resetDrawing);
+}
+
+function draw() {
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let x = i * resolution;
+      let y = j * resolution;
+      if (grid[i][j] == 1) {
+        fill(123, 34, 100);
+        rect(x, y, resolution - 1, resolution - 1);
+      } else {
+        fill(255);
+        strokeWeight(0.25);
+        rect(x, y, resolution - 1, resolution - 1);
+      }
+    }
+  }
+  if (isAnimating) {
+    frameRate(Number(fps));
+    generation++;
+    document.getElementById("stats").innerHTML = "generation: " + generation;
+    for (let i = 0; i < cols; i++) {
+      for (let j = 0; j < rows; j++) {
+        let x = i * resolution;
+        let y = j * resolution;
+        if (grid[i][j] == 1) {
+          fill(123, 34, 100);
+          rect(x, y, resolution - 1, resolution - 1);
+        } else {
+          fill(255);
+          rect(x, y, resolution - 1, resolution - 1);
+        }
+      }
+    }
+    let next = make2DArray(cols, rows);
+    // Next Generation...
+    for (let i = 0; i < cols; i++) {
+      for (let j = 0; j < rows; j++) {
+        let state = grid[i][j];
+        // Live neighbors...
+        let sum = 0;
+        let neighbors = countNeighbors(grid, i, j);
+        if (state == 0 && neighbors == 3) {
+          next[i][j] = 1;
+        } else if (state == 1 && (neighbors < 2 || neighbors > 3)) {
+          next[i][j] = 0;
+        } else {
+          next[i][j] = state;
+        }
+      }
+    }
+    grid = next;
+  }
+}
+
+function countNeighbors(grid, x, y) {
+  let sum = 0;
+  for (let i = -1; i < 2; i++) {
+    for (let j = -1; j < 2; j++) {
+      let col = (x + i + cols) % cols;
+      let row = (y + j + rows) % rows;
+
+      sum += grid[col][row];
+    }
+  }
+  sum -= grid[x][y];
+  return sum;
+}
+
+// GRID COLORING WITH MOUSE
+function mousePressed() {
+  let i = floor(mouseX / resolution);
+  let j = floor(mouseY / resolution);
+
+  if (i >= 0 && i < cols && j >= 0 && j < rows) {
+    grid[i][j] = 1 - grid[i][j];
+  }
+}
+
+// BUTTONS
+function startDrawing() {
+  isAnimating = !isAnimating;
+}
+
+function stopDrawing() {
+  isAnimating = false;
+}
+
+function resetDrawing() {
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      grid[i][j] = 0;
+    }
+  }
+  generation = 0;
+  document.getElementById("stats").innerHTML = "generation: " + generation;
+  isAnimating = false;
+}
+
+// SETTINGS
+document.addEventListener("DOMContentLoaded", function () {
+  var slider = document.getElementById("fps-slider");
+
+  var fpsValue = document.getElementById("fps-value");
+
+  slider.addEventListener("input", function () {
+    fpsValue.textContent = slider.value;
+    fps = parseInt(slider.value);
+  });
+});
